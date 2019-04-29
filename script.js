981c5d3bfbe9ba8dcffcc81af5a126037d018ae3
Th13um <dthieum@gmail.com>
2019-04-29 21:10:17 +0200

    add enter effect for skill logo

--- script.js
+++ script.js
@@ -6,11 +6,11 @@ $(document).ready(function() {
         /* Check the location of each desired element */
         $('.hideme').each( function(i){
 
-            var bottom_of_object = $(this).offset().top + (($(this).height())*20/100);
-            var bottom_of_window = $(window).scrollTop() + $(window).height();
+            var bottomOfObject = $(this).offset().top + (($(this).height())*20/100);
+            var bottomOfWindow = $(window).scrollTop() + $(window).height();
 
             /* If the object is completely visible in the window, fade it it */
-            if( bottom_of_window > bottom_of_object ){
+            if( bottomOfWindow > bottomOfObject){
 
                 $(this).animate({'opacity':'1'},1500);
 
