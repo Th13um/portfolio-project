21bacaba67c4588096d4deaca1f8a01c2439c6b1
Th13um <dthieum@gmail.com>
2019-04-29 22:22:34 +0200

    beautify code

--- script.js
+++ script.js
@@ -1,34 +1,26 @@
 $(document).ready(function() {
-
-    /* Every time the window is scrolled ... */
-    $(window).scroll( function(){
-
-        /* Check the location of each desired element */
-        $('.hideme').each( function(i){
-
-            var bottomOfObject = $(this).offset().top + (($(this).height())*20/100);
-            var bottomOfWindow = $(window).scrollTop() + $(window).height();
-
-            /* If the object is completely visible in the window, fade it it */
-            if( bottomOfWindow > bottomOfObject){
-
-                $(this).animate({'opacity':'1'},1500);
-
-            }
-
-        });
-
+  /* Every time the window is scrolled ... */
+  $(window).scroll(function() {
+    /* Check the location of each desired element */
+    $('.hideme').each(function(i) {
+      var bottomOfObject = $(this).offset().top + (($(this).height()) * 20 / 100);
+      var bottomOfWindow = $(window).scrollTop() + $(window).height();
+      /* If the object is completely visible in the window, fade it it */
+      if (bottomOfWindow > bottomOfObject) {
+        $(this).animate({
+          'opacity': '1'
+        }, 1500);
+      }
     });
-
+  });
 });
 
 
 $(window).scroll(function() {
-    var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-    if (scrollTop > 100) {
-        $('div.bg-1').css('opacity',0);
-    }
-    else {
-        $('div.bg-1').css('opacity',1-scrollTop/100);
-    }
+  var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+  if (scrollTop > 100) {
+    $('div.bg-1').css('opacity', 0);
+  } else {
+    $('div.bg-1').css('opacity', 1 - scrollTop / 100);
+  }
 });
